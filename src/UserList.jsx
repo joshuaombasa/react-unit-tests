2aa45d9e3cc1dec6d8467a1a8118d3a512d0bd84
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-11 23:56:58 +0300

    add build component

---
+++ src/UserList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export default function UserList({users}) {
+
+    const usersList = users.map(user => (
+        <tr key={user.name}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+        </tr>
+    ))
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {usersList}
+            </tbody>
+        </table>
+    )
+}
