445730517c5ec49957b6744e873390f5d3befcff
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-11 23:57:29 +0300

    add build component

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Home() {
+    return (
+        <div className="home-page">
+            <h1>This is home page</h1>
+            <h1>This is home page</h1>
+            <h1>This is home page</h1>
+            <p>Joshua</p>
+            <p>Sheila Ombasa</p>
+            <p>Joy Ombasa</p>
+            <button>Load more</button>
+        </div>
+    )
+}
