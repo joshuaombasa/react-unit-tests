b8e2d9c88bb2cd138b4634c2c369fb9e204a993c
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-12 18:13:31 +0300

    updated component test suite

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import UserForm from './UserForm'
@@ -19,5 +19,39 @@ test('is shows two inputs and a button', () => {
 })
 
 test('it calls addUser when the form is submitted', () => {
-    
+    const mock = jest.fn();
+    render(<UserForm addUser={mock}/>);
+
+    const nameInput = screen.getByRole('textbox', {name: /name/i})
+    const emailInput = screen.getByRole('textbox', {name: /email/i})
+
+   
+    fireEvent.change(nameInput, { target: { value: 'John' } });
+    fireEvent.change(emailInput, { target: { value: 'johnOmbusuro@gmail.com' } });
+
+
+    const button = screen.getByRole('button', { name: /add user/i });
+
+    fireEvent.click(button);
+
+    expect(mock).toHaveBeenCalled()
+    expect(mock).toHaveBeenCalledWith({name: 'John', email: 'johnOmbusuro@gmail.com'})
+})
+
+test('it empties the two inputs when the form is submitted', () => {
+    const mock = jest.fn()
+
+    render(<UserForm  addUser={mock}/>)
+
+    const nameInput = screen.getByRole('textbox', {name: /name/i})
+    const emailInput = screen.getByRole('textbox', {name: /email/i})
+    const button = screen.getByRole('button', {name: /add user/i})
+
+    fireEvent.change(nameInput, {target: {value: 'john'}})
+    fireEvent.change(emailInput, {target: {value: 'joshuaOmbasa@gmail.com'}})
+
+    fireEvent.click(button)
+
+    expect(nameInput).toHaveValue('')
+    expect(emailInput).toHaveValue('')
 })
